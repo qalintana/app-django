14589bdf0279197ee326d463669a116f58c92211
Queiroz Santana <alintana18@gmail.com>
2024-08-18 11:12:57 +0100

    database

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/postgres',
+        default= os.environ.get('DATABASE_URL', default='postgresql://postgres:postgres@localhost:5432/postgres'),
         conn_max_age=600
     )
 }
